7ef10be6ad4737497a9d13e7bd60ea085d23ec1c
ovrocha-design <ovrocha@gmail.com>
2025-09-26 22:32:02 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int numero;
+        for (numero = 100; numero > 1; numero -= 1) {
+            System.out.println(numero + " ");
+
+
+        }
+    }
+}
